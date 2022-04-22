41d76aed8e2a646febf541e7ef9b0620406b16af
abcdandroid2 <ahmad.banikamali@gmail.com>
2022-04-23 00:34:22 +0430

    Pager Config 2

--- app/src/main/java/com/abcdandroid/lenschipset/LensChipset.kt
+++ app/src/main/java/com/abcdandroid/lenschipset/LensChipset.kt
@@ -2,23 +2,27 @@ package com.abcdandroid.lenschipset
 
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.rememberSplineBasedDecay
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import dev.chrisbanes.snapper.ExperimentalSnapperApi
+import dev.chrisbanes.snapper.SnapperLayoutInfo
+import dev.chrisbanes.snapper.SnapperLayoutItemInfo
 import dev.chrisbanes.snapper.rememberSnapperFlingBehavior
 
 //https://medium.com/tech-takeaways/how-to-snap-scrollable-items-to-the-center-of-the-screen-in-jetpack-compose-with-snapper-82a4cc70aebe
@@ -27,11 +31,17 @@ import dev.chrisbanes.snapper.rememberSnapperFlingBehavior
 @Composable
 fun LensChipset(
     chipList: List<String> = listOf(
-        "jklmnop",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
         "ar",
         "bcd",
         "efghi",
-        "ab",
+        "efghiefghief",
+       "ab",
         "bcd",
         "efghi",
         "bcd",
@@ -43,49 +53,68 @@ fun LensChipset(
         "efghi",
         "ab",
         "bcd",
-        "efghi"
+        "efghi" /**/
     )
 ) {
-    Column(modifier = Modifier.height(500.dp)) {
+    Box(modifier = Modifier.height(500.dp)) {
 
         val state = rememberLazyListState()
+        var paddingValue by remember {
+            mutableStateOf(0.dp)
+        }
+
         LazyRow(
             Modifier,
             flingBehavior = rememberSnapperFlingBehavior(
                 lazyListState = state,
-                snapOffsetForItem = { _, _ -> 100 },
-                endContentPadding = 20.dp,
+                snapOffsetForItem = { snapperLayoutInfo:SnapperLayoutInfo, snapLayoutItemInfo:SnapperLayoutItemInfo ->
+                    println(snapLayoutItemInfo.toString())
+                    println(snapperLayoutInfo.toString())
+                    println("===")
+                    paddingValue.value.toInt()
+                },
+                endContentPadding = (0).dp,
                 decayAnimationSpec = rememberSplineBasedDecay(),
                 springAnimationSpec = tween(durationMillis = 100),
-                maximumFlingDistance = { a -> 50f }
             ),
             state = state
         ) {
 
-            items(items = chipList) {
+            itemsIndexed(items = chipList) {index,it->
                 BoxWithConstraints {
-                    ChipItem(it = it)
+                    ChipItem(it = it, isFirst = index==0, paddingValue = 80.dp, boxWithConstraintsScope = {
+                        paddingValue=it.minWidth
+                    })
                 }
 
             }
         }
-        ChipItem(it = "efghi", modifier = Modifier)
+
+        Canvas(modifier = Modifier.fillMaxWidth(), onDraw = {
+            drawLine(Color.Yellow, start = Offset(0f,120f), end = Offset(paddingValue.value,120f), strokeWidth = 50f)
+            drawLine(Color.Yellow, start = Offset(0f,190f), end = Offset(200.dp.toPx(),190f), strokeWidth = 50f)
+        })
+
+
     }
 }
 
 @Composable
-private fun ChipItem(modifier: Modifier = Modifier, it: String) {
+private fun ChipItem(modifier: Modifier = Modifier, it: String,isFirst: Boolean =false,boxWithConstraintsScope: (BoxWithConstraintsScope)->Unit,paddingValue : Dp) {
     val fontSize = 20.sp
-    Text(
-        text = it,
-        fontSize = fontSize,
-        color = Color.White,
-        modifier = modifier
-            .padding(30.dp)
-            .background(
-                shape = RoundedCornerShape(size = 20.dp),
-                color = Color.Black
-            )
-            .padding(vertical = 10.dp, horizontal = 30.dp)
-    )
+    BoxWithConstraints {
+        boxWithConstraintsScope(this)
+        Text(
+            text = it,
+            fontSize = fontSize,
+            color = Color.White,
+            modifier = modifier
+                .padding(start = if (isFirst) paddingValue else 20.dp)
+                .background(
+                    shape = RoundedCornerShape(size = 20.dp),
+                    color = Color.Black
+                )
+                .padding(vertical = 10.dp, horizontal = 30.dp)
+        )
+    }
 }
