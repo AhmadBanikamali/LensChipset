527bd61f229cdcb88f16e176fbdd8aa5eb75026f
abcdandroid2 <ahmad.banikamali@gmail.com>
2022-04-23 15:27:05 +0430

    Pager Config 3

--- app/src/main/java/com/abcdandroid/lenschipset/LensChipset.kt
+++ app/src/main/java/com/abcdandroid/lenschipset/LensChipset.kt
@@ -6,17 +6,19 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.onPlaced
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.accompanist.pager.ExperimentalPagerApi
@@ -26,6 +28,7 @@ import dev.chrisbanes.snapper.SnapperLayoutItemInfo
 import dev.chrisbanes.snapper.rememberSnapperFlingBehavior
 
 //https://medium.com/tech-takeaways/how-to-snap-scrollable-items-to-the-center-of-the-screen-in-jetpack-compose-with-snapper-82a4cc70aebe
+@ExperimentalComposeUiApi
 @ExperimentalSnapperApi
 @ExperimentalPagerApi
 @Composable
@@ -37,84 +40,99 @@ fun LensChipset(
         "4",
         "5",
         "6",
-        "ar",
-        "bcd",
-        "efghi",
-        "efghiefghief",
-       "ab",
-        "bcd",
-        "efghi",
-        "bcd",
-        "efghi",
-        "ab",
-        "bcd",
-        "efghi",
-        "bcd",
-        "efghi",
-        "ab",
-        "bcd",
-        "efghi" /**/
+        "7",
+        "8",
+        "9",
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+        "15",
+        "16",
+        "17",
+        "18",
+        "19",
+        "20",
+        "21",
+        "22",
+        "23" /**/
     )
 ) {
     Box(modifier = Modifier.height(500.dp)) {
 
         val state = rememberLazyListState()
-        var paddingValue by remember {
-            mutableStateOf(0.dp)
-        }
+
+        val paddingValueInPx = 500f
+        val paddingValueInDp = paddingValueInPx.toDp()
+
+        println(state.firstVisibleItemScrollOffset)
 
         LazyRow(
-            Modifier,
+            Modifier
+                .onPlaced {
+
+                }
+                .onGloballyPositioned {
+
+                },
             flingBehavior = rememberSnapperFlingBehavior(
                 lazyListState = state,
-                snapOffsetForItem = { snapperLayoutInfo:SnapperLayoutInfo, snapLayoutItemInfo:SnapperLayoutItemInfo ->
-                    println(snapLayoutItemInfo.toString())
-                    println(snapperLayoutInfo.toString())
-                    println("===")
-                    paddingValue.value.toInt()
+                snapOffsetForItem = { snapperLayoutInfo: SnapperLayoutInfo, snapLayoutItemInfo: SnapperLayoutItemInfo ->
+
+                   paddingValueInPx.toInt()
                 },
                 endContentPadding = (0).dp,
                 decayAnimationSpec = rememberSplineBasedDecay(),
                 springAnimationSpec = tween(durationMillis = 100),
+                maximumFlingDistance = {5f}
             ),
-            state = state
-        ) {
+            state = state,
 
-            itemsIndexed(items = chipList) {index,it->
+            ) {
+            itemsIndexed(items = chipList) { index, it ->
                 BoxWithConstraints {
-                    ChipItem(it = it, isFirst = index==0, paddingValue = 80.dp, boxWithConstraintsScope = {
-                        paddingValue=it.minWidth
-                    })
+                    Text(
+                        text = it,
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier
+                            .onPlaced {
+                                println(it.size)
+                            }
+                            .padding(start = if (index == 0) paddingValueInDp.dp else 0.dp)
+                            .background(
+                                shape = RoundedCornerShape(size = 20.dp),
+                                color = Color.Black
+                            )
+                            .padding(vertical = 10.dp, horizontal = 30.dp)
+                            .drawWithContent {
+                                this.drawContent()
+                            }
+                    )
                 }
-
             }
         }
 
         Canvas(modifier = Modifier.fillMaxWidth(), onDraw = {
-            drawLine(Color.Yellow, start = Offset(0f,120f), end = Offset(paddingValue.value,120f), strokeWidth = 50f)
-            drawLine(Color.Yellow, start = Offset(0f,190f), end = Offset(200.dp.toPx(),190f), strokeWidth = 50f)
+            drawLine(
+                Color.Yellow,
+                start = Offset(0f, 120f),
+                end = Offset(paddingValueInPx, 120f),
+                strokeWidth = 50f
+            )
+            drawLine(
+                Color.Yellow,
+                start = Offset(0f, 190f),
+                end = Offset(100.dp.toPx(), 190f),
+                strokeWidth = 50f
+            )
         })
-
-
     }
 }
 
 @Composable
-private fun ChipItem(modifier: Modifier = Modifier, it: String,isFirst: Boolean =false,boxWithConstraintsScope: (BoxWithConstraintsScope)->Unit,paddingValue : Dp) {
-    val fontSize = 20.sp
-    BoxWithConstraints {
-        boxWithConstraintsScope(this)
-        Text(
-            text = it,
-            fontSize = fontSize,
-            color = Color.White,
-            modifier = modifier
-                .padding(start = if (isFirst) paddingValue else 20.dp)
-                .background(
-                    shape = RoundedCornerShape(size = 20.dp),
-                    color = Color.Black
-                )
-                .padding(vertical = 10.dp, horizontal = 30.dp)
-        )
-    }
-}
+fun Float.toDp() = this / LocalContext.current.resources.displayMetrics.density
+
+@Composable
+fun Float.toPx() = this * LocalContext.current.resources.displayMetrics.density
